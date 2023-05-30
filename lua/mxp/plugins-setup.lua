95bdb14379e48bfb81b9560b339713711971d3fc
mxp <maxplayer@free.fr>
2023-05-30 23:11:05 +0200

    ajout des linter formatter

--- lua/mxp/plugins-setup.lua
+++ lua/mxp/plugins-setup.lua
@@ -1,13 +1,13 @@
 -- s'assure que packer est installé, sinon il l'installe
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
 end
 
 local packer_bootstrap = ensure_packer()
@@ -22,41 +22,54 @@ vim.cmd([[
 
 local status, packer = pcall(require, "packer")
 if not status then
-  return
+	return
 end
 
-return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim' -- packer se gere lui meme comme plugin
-  use("bluz71/vim-nightfly-guicolors") -- preferred colorscheme
-  use("christoomey/vim-tmux-navigator") -- gere tmux et la navigation entre les windows avec ctrl hjkl
-  use("szw/vim-maximizer") -- maximize et restore la fenetre courrante
-  use("tpope/vim-surround") -- pour gérer le surround par des caracteres speciaux
-  use("vim-scripts/ReplaceWithRegister") -- remplace un groupe de mot par quelque chose du registre
-  use("numToStr/Comment.nvim") -- raccourci pour rajouter des commentaires avec gc
-  use("nvim-tree/nvim-tree.lua") -- gestionnaire de fichier
-  use("kyazdani42/nvim-web-devicons") -- un pack d'icone pour vim-tree
-  use("nvim-lualine/lualine.nvim") -- barre de neovim stylé
--- en prerequis il faut installer ripgrep
-  use({'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }) -- pour la recherche dans les fichiers
-  use({'nvim-telescope/telescope.nvim', tag = '0.1.1', requires = { {'nvim-lua/plenary.nvim'} }}) -- telescope pour la recherche de fichier, il ya une dependenace a plenary pour les call async
-  use("hrsh7th/nvim-cmp") -- autocompletion
-  use("hrsh7th/cmp-buffer") -- permet de choisir des elements du buffer pour lautocompletion
-  use("hrsh7th/cmp-path") -- la meme qu'au dessus, mais avec des elements du path
-  use("L3MON4D3/LuaSnip") -- moteur pour l'utilisation des snippets
-  use("saadparwaiz1/cmp_luasnip") -- l'autocompletion
-  use("rafamadriz/friendly-snippets") -- des snippets multi language
-  use("williamboman/mason.nvim") -- gestion et installation serveur lsp
-  use("williamboman/mason-lspconfig.nvim") -- fait le lien entre mason et nvim-lspconfig
-  use("neovim/nvim-lspconfig") -- configuration serveur lsp
-  use("hrsh7th/cmp-nvim-lsp")  -- une sorte dextended completion
-  use({"glepnir/lspsaga.nvim", branch = "main"}) -- ameliore lspconfig
-  -- il faut installer nerdfonts dans ~/.local/share/fonts pour que cela fonctionne correctement
-  use("onsails/lspkind.nvim") -- des icones a la vscode
+return require("packer").startup(function(use)
+	use("wbthomason/packer.nvim") -- packer se gere lui meme comme plugin
+	use("bluz71/vim-nightfly-guicolors") -- preferred colorscheme
+	use("christoomey/vim-tmux-navigator") -- gere tmux et la navigation entre les windows avec ctrl hjkl
+	use("szw/vim-maximizer") -- maximize et restore la fenetre courrante
+	use("tpope/vim-surround") -- pour gérer le surround par des caracteres speciaux
+	use("vim-scripts/ReplaceWithRegister") -- remplace un groupe de not par quelque chose du registre
+	use("numToStr/Comment.nvim") -- raccourci pour rajouter des commentaires avec gc
+	use("nvim-tree/nvim-tree.lua") -- gestionnaire de fichier
+	use("kyazdani42/nvim-web-devicons") -- un pack d'icone pour vim-tree
+	use("nvim-lualine/lualine.nvim") -- barre de neovim stylé
+	-- en prerequis il faut installer ripgrep
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- pour la recherche dans les fichiers
+	use({ "nvim-telescope/telescope.nvim", tag = "0.1.1", requires = { { "nvim-lua/plenary.nvim" } } }) -- telescope pour la recherche de fichier, il ya une dependenace a plenary pour les call async
+	use("hrsh7th/nvim-cmp") -- autocompletion
+	use("hrsh7th/cmp-buffer") -- permet de choisir des elements du buffer pour lautocompletion
+	use("hrsh7th/cmp-path") -- la meme qu'au dessus, mais avec des elements du path
+	use("L3MON4D3/LuaSnip") -- motor pour l'utilisation des snippets
+	use("saadparwaiz1/cmp_luasnip") -- l'autocompletion
+	use("rafamadriz/friendly-snippets") -- des snippets multi language
+	use("williamboman/mason.nvim") -- gestion et installation serveur lsp, linters et les formatters
+	use("williamboman/mason-lspconfig.nvim") -- fait le lien entre mason et nvim-lspconfig
+	use("neovim/nvim-lspconfig") -- configuration serveur lsp
+	use("hrsh7th/cmp-nvim-lsp") -- une sorte dextended completion
+	use({ "glepnir/lspsaga.nvim", branch = "main" }) -- ameliore lspconfig
+	-- il faut installer nerdfonts dans ~/.local/share/fonts pour que cela fonctionne correctement
+	use("onsails/lspkind.nvim") -- des icones a la vscode
+	use("jose-elias-alvarez/null-ls.nvim") -- permet d'injecter du code non LSP via lua
+	use("jayp0521/mason-null-ls.nvim") -- bride entre mason et null-ls
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = function()
+			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+			ts_update()
+		end,
+	}) -- lspsaga semble se plaindre sans ce plugin, sert a creer des parsers
+	-- le binaire treesitter se trouve https://github.com/tree-sitter/tree-sitter/releases
+	-- auto closing
+	use("windwp/nvim-autopairs") -- autoclose parens, brackets, quotes, etc...
+	use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
 
-  -- met en place directement la config apres avoir clone packer.nvim
-  -- mettre ca a apres les lignes de chargement des plugins
+	-- met en place directement la config apres avoir clone packer.nvim
+	-- mettre ca a apres les lignes de chargement des plugins
 
-  if packer_bootstrap then
-    require('packer').sync()
-  end
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
--- lua/mxp/plugins/lsp/mason.lua
+++ lua/mxp/plugins/lsp/mason.lua
@@ -1,33 +1,56 @@
 local mason_status, mason = pcall(require, "mason")
 if not mason_status then
-  return
+	return
 end
 
 local mason_lspconfig_status, mason_lspconfig = pcall(require, "mason-lspconfig")
 if not mason_lspconfig_status then
-  return
+	return
 end
 
+-- import mason-null-ls plugin safely
+local mason_null_ls_status, mason_null_ls = pcall(require, "mason-null-ls")
+if not mason_null_ls_status then
+	return
+end
+
+-- on active mason
 mason.setup()
 
 mason_lspconfig.setup({
-  -- la liste des serveurs que mason doit installer
-  ensure_installed = {
-    "lua_ls",
-    "tsserver",
-    "html",
-    "ansiblels",
-    "bashls",
-    "dockerls",
-    "docker_compose_language_service",
-    "jsonls",
-    "tsserver",
-    "marksman",
-    "pyright",
-    "sqlls",
-    "lemminx",
-    "yamlls"
-  },
-  -- auto-install configured servers (with lspconfig)
-  automatic_installation = true, -- not the same as ensure_installed
+	-- la liste des serveurs que mason doit installer
+	ensure_installed = {
+		"lua_ls",
+		"tsserver",
+		"html",
+		"ansiblels",
+		"bashls",
+		"dockerls",
+		"docker_compose_language_service",
+		"jsonls",
+		"tsserver",
+		"marksman",
+		"pyright",
+		"sqlls",
+		"lemminx",
+		"yamlls",
+	},
+	-- auto-install configured servers (with lspconfig)
+	automatic_installation = true, -- not the same as ensure_installed
+})
+
+mason_null_ls.setup({
+	-- list of formatters & linters for mason to install
+	ensure_installed = {
+		"prettier", -- ts/js formatter
+		"stylua", -- lua formatter
+		"eslint_d", -- ts/js linter
+		"black", -- formatter python
+		"isort@5.11.5", -- formatter ajout alphabetique des imports pythons
+		"shfmt", -- formatter shell
+		"flake8@5.0.4", -- linter python
+		"codespell", -- linter python et js pour la typo
+	},
+	-- auto-install configured formatters & linters (with null-ls)
+	automatic_installation = true,
 })
